b94541bd0345836540599ac963991248c619a2d9
Divyanshu Sharma <divyanshu14472@gmail.com>
2023-08-04 17:30:51 +0530

    Update views.py
    Comments added

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,36 +10,52 @@
 #     serializer_class = UsersSerializer
 
 
-
+# Mainly sending json responses
 from django.http import JsonResponse
+# Imported both the models
 from .models import Users
 from .models import Data
+# Imported json for deserialization and serialization
 import json
+# Hashlib to convert raw passwd to sha-256
 import hashlib
+# create unique session ids
 from uuid import uuid4
 
 def signup(request):
+    # Function for signup
+    # Get all the data in dictionary
     data = json.loads(request.body.decode('utf-8'))
+    # Get name
     name = data["name"]
+    # Get password
     passwd = data["passwd"]
+    # Hash password to sha256
     passwd = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
+    # Put new data to buffer
     user = Users(name=name, passwd = passwd)
+    # Save data to memory
     user.save()
     return JsonResponse({"test":"re"}, safe=False)
 
 def login(request):
+    # data loaded
     data = json.loads(request.body.decode('utf-8'))
     name = data["name"]
     passwd = data["passwd"]
+    # Hash the raw password
     passwd = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
+    # Search user
     found = Users.objects.filter(name=name).first().__dict__
-    
+    # Vweify passowd
     if (found["passwd"] == passwd):
         update = Users.objects.get(name=name)
+        # Create new Session Token
         sid = uuid4()
         update.SID = sid
         update.save()
         returns = JsonResponse({"Pass": True})
+        # set cookies id and SID
         returns.set_cookie('SID', sid)
         returns.set_cookie('id', update.id)
         return returns
@@ -48,6 +64,7 @@ def login(request):
     
 def verify(request):
     try:
+        # verifying if user is valid so that they don't need to  sign in again and again
         name = (request.COOKIES.get('id'))
         sid = request.COOKIES.get('SID')
         found = Users.objects.filter(id=name).first().__dict__
@@ -61,11 +78,14 @@ def verify(request):
 
 
 def upload_image(request):
+    # Solely save image
     data = Data(image=(request.FILES['file']), user_id=request.COOKIES.get('id'))
     data.save()
+    # Return id where image is stored
     return JsonResponse({"id": data.id})
 
 def save_data(request):
+    # Save the remaining data about events
     data = json.loads(request.body.decode('utf-8'))
     id = data['id']
     event_name = data['event_name']
@@ -81,14 +101,17 @@ def save_data(request):
     return JsonResponse({"saved": True})
 
 def get_data(request):
+    # Get all the data about events
     data = Data.objects.all().values()    
     return JsonResponse({"data": list(data)})
 def get_likes(request):
+    # Get array with likes by the user
     data = Users.objects.filter(id=request.COOKIES.get('id')).first().__dict__
     return JsonResponse({"liked": data['liked'], "id":request.COOKIES.get('id')})
 
 
 def update_likes(request):
+    # Updating likes
     data = json.loads(request.body.decode('utf-8'))
     id = request.COOKIES.get('id')
     users = Users.objects.get(id=id)
@@ -98,6 +121,7 @@ def update_likes(request):
     return JsonResponse({"dsa":"dss"})
 
 def logout(request):
+    # For logout just delete those cookies
     returns = JsonResponse({"logout" : True})
     returns.delete_cookie('id')
     returns.delete_cookie('SID')
