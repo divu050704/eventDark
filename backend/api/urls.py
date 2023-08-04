c83ae1c4a34b1f8b1706a5f7240352602aa1c0fc
Divyanshu Sharma <divyanshu14472@gmail.com>
2023-08-04 17:32:41 +0530

    Update urls.py
    Comments added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,14 +6,21 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-   
+   # Signup path 
     path('signup/', views.signup, name="signup"),
+   # Login path
     path('login/',views.login, name='login'),
+   # Verifying a user
     path('verify/', views.verify, name='verify'),
+   # Upload image of event
     path('upload_image/',views.upload_image, name='upload_image'),
+   # Save data of the image uploaded earlier
     path('save_data/',views.save_data, name="save-data"),
+   # Get all the data about events
     path('get_data/',views.get_data, name="get_data"),
+   # Get likes of user
     path('get_likes/',views.get_likes, name='get_likes'),
+   # Update likes of user
     path('update_likes/',views.update_likes, name="update_likes"),
     path('logout/',views.logout, name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
